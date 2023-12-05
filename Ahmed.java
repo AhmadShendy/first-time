b599823c45f85c0eab8592f5a82669e536fcb1e3
AhmadShendy <ahmedshendy387@gmail.com>
2023-12-05 21:40:34 +0200

    Upload

---
+++ Ahmed.java
@@ -0,0 +1,6 @@
+public class Ahmed {
+    public static void main(String[] args) {
+        String name = "Ahmed Shendy";
+        System.out.println("My name is " + name);
+    }
+}
---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM openjdk
+WORKDIR /application
+
+COPY Ahmed.java .
+RUN javac Ahmed.java
+
+CMD java Ahmed
